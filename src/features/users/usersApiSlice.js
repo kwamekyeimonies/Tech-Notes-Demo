aaa342dcfbaf755fa6a0a2b334f31f31a1a0798b
Tenkorang Daniel <kwamekyeimonies@gmail.com>
2023-03-06 20:23:36 +0000

    slice dev

---
+++ src/features/users/usersApiSlice.js
@@ -0,0 +1,26 @@
+import { createEntityAdapter } from "@reduxjs/toolkit";
+import { apiSlice } from "../../app/api/apiSlice";
+
+
+const usersAdapter = createEntityAdapter({})
+const initialState = usersAdapter.getInitialState()
+
+export const usersApiSlice = apiSlice.injectEndpoints({
+    endpoints:builder=>({
+        getUsers:builder.query({
+            query:()=>'/users',
+            validateStatus:(response,result)=>{
+                return response.status === 200 && !result.isError
+            },
+            keepUnusedDataFor:5,
+            transformResponse:responseData=>{
+                const loadedUsers = responseData.map(
+                    (user)=>{
+                        user.id = user._id
+                        return user
+                    }
+                )
+            }
+        })
+    })
+})
